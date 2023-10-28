094db36f43188abae1514aefc97a7f7dac63fd71
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2023-10-28 18:48:47 +0100

    Changed to store FoodbankID instead of a foodbank this solves issue when selecting a new foodbank is crashing

---
+++ GiveFoodUK/Saved/NoFoodBankView.swift
@@ -0,0 +1,18 @@
+//
+//  NoFoodBankView.swift
+//  GiveFoodUK
+//
+//  Created by Nigel Gee on 28/10/2023.
+//
+
+import SwiftUI
+
+struct NoFoodBankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoFoodBankView()
+}
---
+++ GiveFoodUK/Supporting/Collection-IsNotEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  Collection-IsNotEmpty.swift
+//  GiveFoodUK
+//
+//  Created by Nigel Gee on 28/10/2023.
+//
+
+import Foundation
