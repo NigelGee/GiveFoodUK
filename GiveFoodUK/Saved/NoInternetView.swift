1f7e3276ee273efedc5b0a94cdbe423df65af097
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2023-11-01 15:53:40 +0000

    Added NoInternetView(if selected Food bank do not load). Made some UI changes thanks to Al Fraser

---
+++ GiveFoodUK/Saved/NoInternetView.swift
@@ -0,0 +1,18 @@
+//
+//  NoInternetView.swift
+//  GiveFoodUK
+//
+//  Created by Nigel Gee on 01/11/2023.
+//
+
+import SwiftUI
+
+struct NoInternetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoInternetView()
+}
