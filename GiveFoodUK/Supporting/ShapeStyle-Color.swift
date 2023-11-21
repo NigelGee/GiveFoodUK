ad4eef2917636fcf78c998f4f9765724bf27f931
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2023-11-21 15:35:53 +0000

    Change to TitleColor as titleColor is correct

--- GiveFoodUK/Supporting/ShapeStyle-Color.swift
+++ GiveFoodUK/Supporting/ShapeStyle-Color.swift
@@ -10,5 +10,5 @@ import SwiftUI
 extension ShapeStyle where Self == Color {
     static var activeColor: Color { Color(red: 0.894, green: 0.937, blue: 0.976) }
     static var inactiveColor: Color { Color(red: 0.937, green: 0.961, blue: 0.984) }
-    static var titleColor: Color { Color("TitleColor") }
+    static var titleColor: Color { Color("titleColor") }
 }
